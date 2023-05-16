b99e52da4960fb57f5a88feb77070e90b4e62101
Arulselvan Madhavan <arulselvan1234@gmail.com>
2023-05-16 09:10:58 -0700

    Print

--- bin/main.ml
+++ bin/main.ml
@@ -21,5 +21,6 @@ let () =
   let Semant.{ ty; _ } = Semant.trans_prog parsed_exp in
   Ast.sexp_of_comp_unit parsed_exp |> Sexplib0.Sexp.to_string_hum |> Printf.printf "%s\n";
   Printf.printf "Result type:%s\n" (Types.type2str ty);
-  Ir_gen.gen_prog parsed_exp
+  Printf.printf "Generating protobuf output";
+  Ir_gen.gen_prog parsed_exp |> Ir_gen.dump
 ;;
