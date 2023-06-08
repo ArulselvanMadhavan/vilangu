7034abc494d2bf535e4db26b5e451d663d3d9c19
arul <arul@aruls-MacBook-Pro.local>
2023-06-08 14:08:19 -0700

    Fix groupname issues

--- run.sh
+++ run.sh
@@ -4,6 +4,8 @@ PROJECT_HOME=$(pwd)
 cd ${PROJECT_HOME}/backend/
 mkdir -p build
 cd build
+export CXX=/usr/bin/clang++
+export CC=/usr/bin/clang
 cmake ../ && make
 cd ${PROJECT_HOME}
 dune exec vilangu
